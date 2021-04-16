6335defa01c7a35cf9a4ba51d9099d871fc85cf9
Juan Andrés Gonzalez Arias <jgonzalezar@unal.edu.co>
2021-04-15 23:24:25 -0500

    CASITO 2.4

--- Tarea2/2_4.c
+++ Tarea2/2_4.c
@@ -2,35 +2,54 @@
 
 // Variables del sistema
 
-#define EVENT_ARRIVAL_AER 1 //Llegada pasajero aeropuerto
-#define EVENT_ARRIVAL_B1 2	//Llegada del bus 1
-#define EVENT_ARRIVAL_H1 3	//Llegada pasajero hotel 1
-#define EVENT_ARRIVAL_B2 4	//Llegada del bus 2
-#define EVENT_ARRIVAL_H2 5	//Llegada pasajero hotel 2
+#define EVENT_PASS_AER 1   //Llegada pasajero aeropuerto
+#define EVENT_PASS_H1 2    //Llegada pasajero Hotel 1
+#define EVENT_PASS_H1 3    //Llegada pasajero Hotel 2
+#define EVENT_BUS1_AER 4    //Llegada bus 1 aeropuerto
+#define EVENT_BUS1_H1 5     //Llegada bus 1 Hotel 1
+#define EVENT_BUS1_H2 6     //Llegada bus 1 Hotel 2
+#define EVENT_BUS2_AER 7    //Llegada bus 2 aeropuerto
+#define EVENT_BUS2_H1 8     //Llegada bus 2 Hotel 1
+#define EVENT_BUS2_H2 9     //Llegada bus 2 Hotel 2
 #define LIST_QUEUE_AER 1	//Cola aeropuerto
 #define LIST_QUEUE_H1 2		//Cola hotel 1
 #define LIST_QUEUE_H2 3		//Cola hotel 2
-#define SAMPST_DELAYS 1
-#define STREAM_INTERARRIVAL 1
-#define STREAM_SERVICE 2
+#define LIST_QUEUE_B1_H 4		//Cola bus 1
+#define LIST_QUEUE_B2_H 5		//Cola bus 2
+#define LIST_QUEUE_B1_AER 6		//Cola bus 1 aeropuerto
+#define LIST_QUEUE_B2_AER 7		//Cola bus 2
 
-int par_b1, par_b2, personas_B1, personas_B2, personas_A_B1, personas_A_B2, asientos_B1, asientos_B2, ending;
+#define BUS_IN_AER 1
+#define BUS_IN_H1 2
+#define BUS_IN_H2 3
+
+#define SAMPST_DELAYS 1		//Cola bus 1
+#define STREAM_INTERARRIVAL 1
+// #define STREAM_SERVICE 2
+
+int estado_b1, estado_b2, asientos_B1, asientos_B2, ending;
 float mean_interarrival_par, mean_interarrival_aer, mean_interarrival_bustop, desviacion_par;
 FILE *infile, *outfile;
 
 void init_model(void);
-void arrive_B1(int par); //Evento llegada
-void arrive_B2(int par); //Evento llegada
-void arrive_H1(void);
-void arrive_H2(void);
-void arrive_Aer(void);
+void arrive_pass_h1(void);
+void arrive_pass_h2(void);
+void arrive_pass_aer(void);
+void arrive_bus1_h1(void);
+void arrive_bus1_h2(void);
+void arrive_bus1_aer(void);
+void arrive_bus2_h1(void);
+void arrive_bus2_h2(void);
+void arrive_bus2_aer(void);
+
+float normal(float mean, float std);
 void report(void); //Generación del reporte
 
 int main() //Programa principal
 {
 	infile = fopen("2_4.in", "r");
 	outfile = fopen("2_4.out", "w");
-	fscanf(infile, "%f %f %d %d %f %f %f %f %d %d %f %f %d", &mean_interarrival_par, &mean_interarrival_bustop, &mean_interarrival_aer, &desviacion_par, &ending);
+	fscanf(infile, "%f %f %f %f %d %d %d", &mean_interarrival_par, &mean_interarrival_bustop, &mean_interarrival_aer, &desviacion_par, &asientos_B1, &asientos_B2, &ending);
 	fprintf(outfile, "Media de tiempo para llegar a cada parada %.2f minutes\n", mean_interarrival_par);
 	fprintf(outfile, "Desviación estandar de las llegadas a cada parada %.2f minutes\n", desviacion_par);
 	fprintf(outfile, "Media de tiempo de llegada de los vuelos %.2f minutes\n", mean_interarrival_aer);
@@ -44,20 +63,40 @@ int main() //Programa principal
 		timing();
 		switch (next_event_type)
 		{
-		case EVENT_ARRIVAL_AER:
-			arrive_Aer();
+		case EVENT_PASS_AER:
+			arrive_pass_aer();
 			break;
-		case EVENT_ARRIVAL_H1:
-			arrive_H1();
+
+		case EVENT_PASS_H1:
+			arrive_pass_h1();
 			break;
-		case EVENT_ARRIVAL_H2:
-			arrive_H2();
+
+		case EVENT_PASS_H2:
+			arrive_pass_h2();
 			break;
-		case EVENT_ARRIVAL_B1:
-			arrive_B1();
+
+		case EVENT_BUS1_AER:
+			arrive_bus1_aer();
 			break;
-		case EVENT_ARRIVAL_B2:
-			arrive_B2();
+
+		case EVENT_BUS1_H1:
+			arrive_bus1_h1();
+			break;
+
+		case EVENT_BUS1_H2:
+			arrive_bus1_h2();
+			break;
+
+		case EVENT_BUS2_AER:
+			arrive_bus2_aer();
+			break;
+
+		case EVENT_BUS2_H1:
+			arrive_bus2_h1();
+			break;
+
+		case EVENT_BUS2_H2:
+			arrive_bus2_h2();
 			break;
 		}
 	}
@@ -69,160 +108,259 @@ int main() //Programa principal
 
 void init_model(void)
 {
-	personas_B1 = 0;
-	personas_B2 = 0;
-	personas_A_B1 = 0;
-	personas_A_B2 = 0;
-	asientos_B1 = 0;
-	asientos_B2;
-	par_b1 = 2;
-	event_schedule(normal(mean_interarrival_par, desviacion_par), EVENT_ARRIVAL_B1);
-
-	//faltaría que el bus 2 iniciara 30 min después del primero
-	
+	//par_b1 = 2;
+	float init_b1 = normal(mean_interarrival_par, desviacion_par);
+	event_schedule(sim_time + init_b1, EVENT_ARRIVAL_B1);
+	event_schedule(sim_time + init_b1 + 30, EVENT_ARRIVAL_B2);
+	event_schedule(sim_time + expon(mean_interarrival_aer),EVENT_ARRIVAL_AER);
+	event_schedule(sim_time + expon(mean_interarrival_bustop,STREAM_INTERARRIVAL),EVENT_ARRIVAL_H1);
+	event_schedule(sim_time + expon(mean_interarrival_bustop,STREAM_INTERARRIVAL),EVENT_ARRIVAL_H2);
 }
 
-//llegada del bus 1
-void arrive_B1(void)
-{
-	event_schedule(sim_time + mean_interarrival_par, EVENT_ARRIVAL_B1);
-	//está en el hotel 1
-	if (par_b1 == 0)
+void arrive_bus1_aer(void){
+	if (estado_b1 != BUS_IN_AER)
 	{
-		//apunta a la siguiente parada (hotel 2)
-		++par_b1;
-		int aux;
-
-		// int size_queue_h1 = list_size(LIST_QUEUE_H1);
-
-		//las personas en el bus 1 ahora son la mitad de las que venían del aeropuerto
-		personas_B1 -= (personas_A_B1 / 2);
-
-		//las personas que venían del aeropuerto ahora son la mitad
-		personas_A_B1 /= 2;
-
-		//for para remover todas las personas que están en la cola del paradero de bus del hotel 1
-		for (aux = 0; aux < list_size[LIST_QUEUE_H1]; aux++)
+		estado_b1 = BUS_IN_AER;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS1_AER);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		
+		while (list_size[LIST_QUEUE_B1_AER] > 0)
 		{
-			list_remove(FIRST, LIST_QUEUE_H1);
+			list_remove(FIRST, LIST_QUEUE_B1_AER);
 		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
 
-		// se agregan las paersonas que estaban en la cola dell paradero de bus del hotel 1 al tota, de personas en el bus 1
-		personas_B1 += aux;
-
-		// se calculan las demoras (creo jejeje)
-		sampst(sim_time, SAMPST_DELAYS);
-
-		// está en la parada hotel 2
-	}
-	else if (par_b1 == 1)
-	{
-		//apunta a la parada del aerpuerto
-		++par_b1;
-		int aux;
-
-		//las personas que venían del aeropuerto se bajan en el hotel 2
-		personas_B1 -= personas_A_B1;
-		//ya no hay personas que venían del aeropuerto
-		personas_A_B1 = 0;
-
-		//for para remover todas las personas que están en la cola del paradero de bus del hotel 2
-		for (aux = 0; aux < list_size[LIST_QUEUE_H2]; aux++)
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B1_AER] + list_size(LIST_QUEUE_B1_H)) < asientos_B1 && list_size[LIST_QUEUE_AER] > 0)
 		{
-			list_remove(FIRST, LIST_QUEUE_H2);
-		}
-
-		//se agregan al total de personas en el bus 1 las que se acaban de subir en el hotel 2
-		personas_B1 += aux;
-
-		//está en el aeropuerto
-	}
-	else if (par_b1 == 2)
-	{
-		//apunta a la parada hotel 1
-		par_b1 = 0;
-		int aux;
-		//for para remover todas las personas que están en la cola del paradero de bus del aeropuerto
-		for (aux = 0; aux < list_size[LIST_QUEUE_AER]; aux++)
-		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
 			list_remove(FIRST, LIST_QUEUE_AER);
+			list_file(FIRST, LIST_QUEUE_B1_H);
 		}
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
 
-		//las personas en el bus son las que se acaban de subir al aeropuerto
-		personas_B1 = aux;
-		//las personas que vienen del aeropuerto son las que se acaban de subir
-		personas_A_B1 = aux;
+		float next_normal;
+		do
+		{
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
+
+		event_schedule(sim_time + next_normal, EVENT_BUS1_H1);
 	}
-
-	//event_schedule(sim_time + interarrival, EVENT_ARRIVAL);
 }
 
-//HACE LO MISMO QUE LA arrive_B1 pero para el bus 2
-void arrive_B2(void)
-{
-	event_schedule(sim_time + mean_interarrival_par, EVENT_ARRIVAL_B2);
-	if (par_b2 == 0)
+void arrive_bus1_h1(void){
+	if (estado_b1 != BUS_IN_H1)
 	{
-		++par_b2;
-		int aux;
-
-		//int size_queue_h1 = list_size(LIST_QUEUE_H1);
-
-		personas_B2 -= (personas_A_B2 / 2);
-		personas_A_B2 /= 2;
-		for (aux = 0; aux < list_size[LIST_QUEUE_H1]; aux)
+		estado_b1 = BUS_IN_H1;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS1_H1);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		int bajados = list_size[LIST_QUEUE_B1_H]/2;
+		while (list_size[LIST_QUEUE_B1_H] > bajados)
 		{
+			list_remove(FIRST, LIST_QUEUE_B1_H);
+		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
+
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B1_AER] + list_size(LIST_QUEUE_B1_H)) < asientos_B1 && list_size[LIST_QUEUE_H1] > 0)
+		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
 			list_remove(FIRST, LIST_QUEUE_H1);
+			list_file(FIRST, LIST_QUEUE_B1_AER);
 		}
-		personas_B2 += aux;
-		sampst(sim_time, SAMPST_DELAYS);
-	}
-	else if (par_b2 == 1)
-	{
-		++par_b2;
-		int aux;
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
 
-		personas_B2 -= personas_A_B2;
-		personas_A_B2 = 0;
-
-		for (aux = 0; aux < list_size[LIST_QUEUE_H2]; aux)
+		float next_normal;
+		do
 		{
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
+
+		event_schedule(sim_time + next_normal, EVENT_BUS1_H2);
+	}
+}
+
+void arrive_bus1_h2(void){
+	if (estado_b1 != BUS_IN_H2)
+	{
+		estado_b1 = BUS_IN_H2;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS1_H2);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		
+		while (list_size[LIST_QUEUE_B1_H] > 0)
+		{
+			list_remove(FIRST, LIST_QUEUE_B1_H);
+		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
+
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B1_AER] + list_size(LIST_QUEUE_B1_H)) < asientos_B1 && list_size[LIST_QUEUE_H2] > 0)
+		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
 			list_remove(FIRST, LIST_QUEUE_H2);
+			list_file(FIRST, LIST_QUEUE_B1_AER);
 		}
-	}
-	else if (par_b2 == 2)
-	{
-		par_b2 = 0;
-		int aux;
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
 
-		for (aux = 0; aux < list_size[LIST_QUEUE_AER]; aux++)
+		float next_normal;
+		do
 		{
-			list_remove(FIRST, LIST_QUEUE_AER);
-		}
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
 
-		personas_B2 = aux;
-		personas_A_B2 = aux;
+		event_schedule(sim_time + next_normal, EVENT_BUS1_H2);
 	}
-
-	//event_schedule(sim_time + interarrival, EVENT_ARRIVAL);
 }
 
-void arrive_H1(void)
-{
-	list_file(LAST, LIST_QUEUE_H1);
-	event_schedule(sim_time + expon(mean_interarrival_bustop, STREAM_INTERARRIVAL), EVENT_ARRIVAL_H1);
+void arrive_bus2_aer(void){
+	if (estado_b2 != BUS_IN_AER)
+	{
+		estado_b2 = BUS_IN_AER;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS2_AER);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		
+		while (list_size[LIST_QUEUE_B2_AER] > 0)
+		{
+			list_remove(FIRST, LIST_QUEUE_B2_AER);
+		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
+
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B2_AER] + list_size(LIST_QUEUE_B2_H)) < asientos_B2 && list_size[LIST_QUEUE_AER] > 0)
+		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
+			list_remove(FIRST, LIST_QUEUE_AER);
+			list_file(FIRST, LIST_QUEUE_B2_H);
+		}
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
+
+		float next_normal;
+		do
+		{
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
+
+		event_schedule(sim_time + next_normal, EVENT_BUS2_H1);
+	}
 }
 
-void arrive_H2(void)
-{
-	list_file(LAST, LIST_QUEUE_H2);
-	event_schedule(sim_time + expon(mean_interarrival_bustop, STREAM_INTERARRIVAL), EVENT_ARRIVAL_H2);
+void arrive_bus2_h1(void){
+	if (estado_b2 != BUS_IN_H1)
+	{
+		estado_b2 = BUS_IN_H1;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS2_H1);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		int bajados = list_size[LIST_QUEUE_B2_H]/2;
+		while (list_size[LIST_QUEUE_B2_H] > bajados)
+		{
+			list_remove(FIRST, LIST_QUEUE_B2_H);
+		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
+
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B2_AER] + list_size(LIST_QUEUE_B2_H)) < asientos_B2 && list_size[LIST_QUEUE_H1] > 0)
+		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
+			list_remove(FIRST, LIST_QUEUE_H1);
+			list_file(FIRST, LIST_QUEUE_B2_AER);
+		}
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
+
+		float next_normal;
+		do
+		{
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
+
+		event_schedule(sim_time + next_normal, EVENT_BUS2_H2);
+	}
 }
 
-void arrive_Aer(void)
+void arrive_bus2_h2(void){
+	if (estado_b2 != BUS_IN_H2)
+	{
+		estado_b2 = BUS_IN_H2;
+		float random_normal = normal(mean_interarrival_par, desviacion_par);
+		printf("First normal: %.2f\n", random_normal);
+		event_schedule(sim_time + random_normal, EVENT_BUS2_H2);
+		printf("A-----------\n");
+		//printf("En el bus al llegar: %d\n", list_size[LIST_PASS_BUS]);
+		
+		while (list_size[LIST_QUEUE_B2_H] > 0)
+		{
+			list_remove(FIRST, LIST_QUEUE_B2_H);
+		}
+		//printf("En el bus después de descargar: %d\n", list_size[LIST_PASS_BUS]);
+
+		//printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		while ((list_size[LIST_QUEUE_B2_AER] + list_size(LIST_QUEUE_B2_H)) < asientos_B2 && list_size[LIST_QUEUE_H2] > 0)
+		{
+			//printf("%d\n",list_size[LIST_PASS_BUS]);
+			list_remove(FIRST, LIST_QUEUE_H2);
+			list_file(FIRST, LIST_QUEUE_B2_AER);
+		}
+		// printf("En el bus después de subir a la gente de A: %d\n", list_size[LIST_PASS_BUS]);
+		// printf("En la fila A: %d\n", list_size[LIST_PASS_A]);
+		// printf("Normal: %.2f\n", normal(bus_mean, bus_std));
+
+		float next_normal;
+		do
+		{
+			next_normal = normal(mean_interarrival_aer, desviacion_par);
+			printf("Next normal: %.2f\n", next_normal);
+		} while (next_normal <= random_normal);
+
+		event_schedule(sim_time + next_normal, EVENT_BUS2_H2);
+	}
+}
+
+void arrive_pass_aer(void)
 {
+	event_schedule(sim_time + expon(mean_interarrival_aer, STREAM_INTERARRIVAL), EVENT_PASS_AER);
 	list_file(LAST, LIST_QUEUE_AER);
-	event_schedule(sim_time + expon(mean_interarrival_aer, STREAM_INTERARRIVAL), EVENT_ARRIVAL_H1);
+}
+
+void arrive_pass_h1(void)
+{
+	event_schedule(sim_time + expon(mean_interarrival_bustop, STREAM_INTERARRIVAL), EVENT_PASS_H1);
+	list_file(LAST, LIST_QUEUE_H1);
+}
+
+void arrive_pass_h2(void)
+{
+	event_schedule(sim_time + expon(mean_interarrival_bustop, STREAM_INTERARRIVAL), EVENT_PASS_H2);
+	list_file(LAST, LIST_QUEUE_H2);
 }
 
 float normal(float mean, float std)
@@ -235,13 +373,6 @@ float normal(float mean, float std)
     return std * sqrt((float)12 / cK) * (aux - (float)cK / 2) + mean;
 }
 
-float poisson(float mean) /* Exponential variate generation function. */
-{
-	int f = rand() % 20;
-	
-	
-	return -mean * log((lcgrand(f)));
-}
 
 
 void report(void)
